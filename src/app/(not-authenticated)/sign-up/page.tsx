b7b958181569668ffc84e5b3e2a05215df1514b3
Pollyana Valverde <valverdepolly@gmail.com>
2026-06-11 14:31:44 -0300

    feat(app, components, styles): implement authentication pages and UI foundation

---
+++ src/app/(not-authenticated)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Hello world sign-up</h1>
+    </div>
+  );
+}
---
+++ src/components/ui/text.tsx
@@ -0,0 +1,42 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const textVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-base leading-[140%] font-normal font-sans",
+      display: "text-4xl leading-[140%] font-extrabold font-sans",
+      "heading-1": "text-2xl leading-[140%] font-extrabold font-sans",
+      "heading-3": "text-lg leading-[140%] font-bold font-sans",
+      content: "text-sm leading-[140%] font-normal font-sans",
+      caption: "text-xs leading-[140%] font-normal font-sans",
+      label: "text-[.625rem] leading-[140%] font-bold uppercase font-mono",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
+
+interface TextProps
+  extends React.HTMLAttributes<HTMLElement>, VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+}
+
+function Text({
+  children,
+  as = "span",
+  className,
+  variant,
+  ...rest
+}: TextProps) {
+  return React.createElement(
+    as,
+    { className: cn(textVariants({ variant }), className), ...rest },
+    children,
+  );
+}
+
+export { Text, textVariants };
